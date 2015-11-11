aa3af6abc40b19327cd525a29af5678c7eb9ef6b
David Bews <david.bews@carleton.ca>
2015-11-10 22:50:28 -0500

    Added Rotational information
    Added it to the screen.

--- app/src/main/java/com/bewmens/ElectronicSwimCoach/CameraActivity.java
+++ app/src/main/java/com/bewmens/ElectronicSwimCoach/CameraActivity.java
@@ -3,10 +3,15 @@ package com.bewmens.ElectronicSwimCoach;
 import com.bewmens.ElectronicSwimCoach.ImageProcessingThread;
 
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceView;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
@@ -20,6 +25,13 @@ import org.opencv.core.Mat;
 public class CameraActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
     private CameraBridgeViewBase mOpenCvCameraView;
+    private SensorManager mSensorManager;
+    private RotationalSensor mRotationalSensor;
+
+    private TextView tvAzimuth;
+    private TextView tvPitch;
+    private TextView tvRoll;
+
     private static final String TAG = "ESC::CameraActivity";
 
 
@@ -48,12 +60,23 @@ public class CameraActivity extends Activity implements CameraBridgeViewBase.CvC
     public void onCreate(Bundle savedInstanceState) {
         Log.i(TAG, "called onCreate");
         super.onCreate(savedInstanceState);
+
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+
+        mRotationalSensor = new RotationalSensor();
+
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_camera);
         mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.HelloOpenCvView);
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
         mOpenCvCameraView.setMaxFrameSize(800, 480);
         mOpenCvCameraView.setCvCameraViewListener(this);
+
+        tvAzimuth = (TextView)findViewById(R.id.textViewZ);
+        tvRoll = (TextView)findViewById(R.id.textViewY);
+        tvPitch = (TextView)findViewById(R.id.textViewX);
+
+        mRotationalSensor.start();
     }
 
     @Override
@@ -62,6 +85,7 @@ public class CameraActivity extends Activity implements CameraBridgeViewBase.CvC
         super.onPause();
         if (mOpenCvCameraView != null)
             mOpenCvCameraView.disableView();
+        mRotationalSensor.stop();
     }
 
     @Override
@@ -75,12 +99,14 @@ public class CameraActivity extends Activity implements CameraBridgeViewBase.CvC
             Log.d(TAG, "OpenCV library found inside package. Using it!");
             mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
         }
+        mRotationalSensor.start();
     }
 
     public void onDestroy() {
         super.onDestroy();
         if (mOpenCvCameraView != null)
             mOpenCvCameraView.disableView();
+        mRotationalSensor.stop();
     }
 
     public void onCameraViewStarted(int width, int height) {
@@ -94,11 +120,80 @@ public class CameraActivity extends Activity implements CameraBridgeViewBase.CvC
         Mat greyFrame = inputFrame.gray();
         Thread thread = new Thread(new ImageProcessingThread(greyFrame));
         thread.start();
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                String azimuth=Float.toString(mRotationalSensor.getAzimuth());
+                String pitch=Float.toString(mRotationalSensor.getPitch());
+                String roll=Float.toString(mRotationalSensor.getRoll());
+
+                tvAzimuth.setText(azimuth);
+                tvPitch.setText(pitch);
+                tvRoll.setText(roll);
+            }
+        });
+
+
         try {
             thread.join();
         } catch (InterruptedException e){
             e.printStackTrace();
         }
+
+
         return greyFrame;
     }
+
+
+    class RotationalSensor implements SensorEventListener {
+
+        private Sensor mRotationVectorSensor;
+        private final float[] mRotationMatrix = new float[16];
+        private final float[] orientation = new float[3];
+
+        public RotationalSensor() {
+            Log.i(TAG, "RotationalSensor Initialized");
+            mRotationVectorSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+            mRotationMatrix[ 0] = 1;
+            mRotationMatrix[ 4] = 1;
+            mRotationMatrix[ 8] = 1;
+            mRotationMatrix[12] = 1;
+        }
+
+        public void start() {
+            Log.i(TAG, "RotationalSensor Started");
+            mSensorManager.registerListener(this, mRotationVectorSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        public void stop() {
+            Log.i(TAG, "RotationalSensor Stopped");
+            mSensorManager.unregisterListener(this);
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            Log.i(TAG, "RotationalSensor Changed");
+            if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
+                SensorManager.getRotationMatrixFromVector(mRotationMatrix , event.values);
+                SensorManager.getOrientation(mRotationMatrix, orientation);
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+
+        public float getAzimuth(){
+            return orientation[0];
+        }
+
+        public float getPitch(){
+            return orientation[1];
+        }
+
+        public float getRoll(){
+            return orientation[2];
+        }
+    }
+
 }
